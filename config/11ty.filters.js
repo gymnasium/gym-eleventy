45818eb44c0575221149c65380f6f67d55c8fdac
Roman Edirisinghe <rediris@users.noreply.github.com>
2023-11-20 12:12:02 -0500

    collections refactoring

---
+++ config/11ty.filters.js
@@ -0,0 +1,122 @@
+// Generic functions
+function replaceAll(string, find, replace) {
+  return string.replace(new RegExp(find, 'g'), replace);
+}
+
+// sort by order
+function sortByOrder(values) {
+  let vals = [...values]; // this *seems* to prevent collection mutation...
+  return vals.sort((a, b) => Math.sign(a.data.order - b.data.order));
+}
+
+module.exports = eleventyConfig => {
+  // DEBUG: Show object keys
+  eleventyConfig.addFilter('object_keys', (obj) => Object.keys(obj));
+
+  // DEBUG: Show object values
+  eleventyConfig.addFilter('object_values', (obj) => Object.values(obj));
+
+  // DEBUG: Show object values
+  eleventyConfig.addFilter('object_entries', (obj) => Object.entries(obj));
+
+  // extract items from objects or arrays
+  eleventyConfig.addFilter('pluck', function (obj, attr, value) {
+    let arr;
+    if (typeof obj === 'object') {
+      arr = Object.values(obj);
+    } else if (typeof obj === 'array') {
+      arr = obj;
+    } else {
+      console.error(`Oops, the pluck filter accepts either an object or an array!`);
+    }
+
+    const filtered = arr.filter((item) => {
+      return item[attr].includes(value);
+    });
+
+    return filtered;
+  });
+
+  eleventyConfig.addFilter('uppercase', (string) => {
+    return string.toUpperCase();
+  });
+  
+  eleventyConfig.addFilter('date', (date, dateFormat) => {
+    return format(date, dateFormat);
+  });
+
+  eleventyConfig.addFilter('stringify', (data) => {
+    return JSON.stringify(data, null, '\t');
+  });
+
+  eleventyConfig.addFilter('strip_html', (post) => {
+    const content = post.replace(/(<([^>]+)>)/gi, '');
+    return content;
+  });
+
+  // Modified, original @ https://willvincent.com/2022/07/27/redirects-with-11ty-and-netlify/
+  eleventyConfig.addFilter('redirect_course', (id, force) => {
+    idString = id.split('-')[1];
+    let idNum = Number(idString);
+    let path;
+    let courseType;
+
+    if (idNum <= 9 || (idNum >= 100 && idNum < 108)) {
+      courseType = 'old';
+    } else {
+      courseType = 'new';
+    }
+
+    if (courseType === 'new' || force === true) {
+      if (force === true) {
+        path = `/learning/course/course-v1:GYM+${idString}+0/`;
+      } else {
+        path = `/courses/course-v1:GYM+${idString}+0/`;
+      }
+    } else if (courseType === 'old') {
+      path = `/courses/GYM/${idString}/0/`;
+    }
+
+    // console.log(idNum, idString, path)
+    return path;
+  });
+
+  eleventyConfig.addFilter('is_string', (obj) => {
+    return typeof obj === 'string';
+  });
+
+  // smart-ish replace filter, which works on strings and objects
+  eleventyConfig.addFilter('replace', (input, string, replace) => {
+    let stringified = typeof input === 'object' ? JSON.stringify(input) : input;
+    let output = JSON.parse(replaceAll(stringified, string, replace));
+    return output;
+  });
+
+  // Modified version, original @https://stevenwoodson.com/blog/a-step-by-step-guide-to-sorting-eleventy-global-data-files-by-date/
+  eleventyConfig.addFilter('sortDataByDate', (obj, dateField = 'date') => {
+      const sorted = {};
+
+      const values = Object.values(obj);
+
+      values
+        .sort((a, b) => {
+          let sortOrder = a[dateField].toISOString() > b[dateField].toISOString();
+          return sortOrder ? 1 : -1;
+        })
+        .forEach((name) => (sorted[name] = values[name]));
+
+      return values;
+    }
+  );
+
+  eleventyConfig.addFilter('sortByOrder', sortByOrder);
+
+
+  eleventyConfig.addFilter('sortByTitle', values => {
+    return values.sort((a, b) => a.title.localeCompare(b.title))
+  })
+
+  eleventyConfig.addFilter('sortByUrl', values => {
+    return values.sort((a, b) => a.url.localeCompare(b.url))
+  })
+}
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,122 +1,49 @@
 // docs: https://www.11ty.io/docs/config/
-const eleventySass = require("eleventy-sass");
-const pluginRev = require("eleventy-plugin-rev");
-const pluginImages = require("./eleventy.config.images.js");
-const yaml = require("js-yaml");
+const eleventySass = require('eleventy-sass');
+const pluginRev = require('eleventy-plugin-rev');
+const filters = require('./config/11ty.filters.js');
+const shortcodes = require('./config/11ty.shortcodes.js');
+const pluginImages = require('./config/11ty.images.js');
+const yaml = require('js-yaml');
 
-module.exports = function(eleventyConfig) {
-  // Generic functions
-  function replaceAll(string, find, replace) {
-    return string.replace(new RegExp(find, 'g'), replace);
-  }
+function filter(arr, criteria) {
+  return arr.filter(function (obj) {
+    return Object.keys(criteria).every(function (c) {
+      return obj[c] == criteria[c];
+    });
+  });
+}
+
+module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({
-    "./public/": "/",
+    './public/': '/',
   });
 
+  eleventyConfig.addPlugin(shortcodes);
+  eleventyConfig.addPlugin(filters);
+  eleventyConfig.addPlugin(pluginRev);
   eleventyConfig.addPlugin(pluginImages);
 
-  eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
-  eleventyConfig.addDataExtension("yml", contents => yaml.load(contents));
-
-  eleventyConfig.addAsyncFilter("uppercase", async function(string) {
-    return string.toUpperCase();
-  });
-
-  eleventyConfig.addShortcode("year", () => `${(new Date()).toLocaleString('en-US', {year: 'numeric'})}`);
-
-  // Modified version, original via @https://www.seanmcp.com/articles/add-a-youtube-embedder-shortcode-to-your-eleventy-site/
-  eleventyConfig.addShortcode("youtube", (embedId, title) => {
-    let id;
-    if (embedId.includes('//')) {
-      const url = new URL(id);
-      id = url.searchParams.get("v");
-    } else {
-      id = embedId;
-    }
-
-    return `
-      <div class="embed-responsive embed-responsive-16by9">
-        <iframe class="yt-shortcode" src="https://www.youtube-nocookie.com/embed/${id}" title="YouTube video player${
-        title ? ` for ${title}` : ""
-      }" frameborder="0" allowfullscreen></iframe>
-      </div>
-    `;
-  });
-
-  eleventyConfig.addFilter('stringify', (data) => {
-    return JSON.stringify(data, null, "\t")
-  });
-
-  eleventyConfig.addFilter("strip_html", (post) => {
-    const content = post.replace(/(<([^>]+)>)/gi, "");
-    return content;
-  });
-
-  // Modified, original @ https://willvincent.com/2022/07/27/redirects-with-11ty-and-netlify/
-  eleventyConfig.addFilter("redirect_course", (id, force) => {
-    idString = id.split('-')[1]
-    let idNum = Number(idString);
-    let path;
-    let format;
-
-    if (idNum <= 9 || ( idNum >= 100 && idNum < 108 ) ) {
-      format = 'old';
-    } else {
-      format = 'new';
-    }
-
-    if (format === 'new' || force === true) {
-      if (force === true) {
-        path = `/learning/course/course-v1:GYM+${idString}+0/`
-      } else {
-        path = `/courses/course-v1:GYM+${idString}+0/`
-      }
-    } else if (format === 'old') {
-      path = `/courses/GYM/${idString}/0/`
-    }
-
-    // console.log(idNum, idString, path)
-    return path;
-  });
-
-  eleventyConfig.addFilter('is_string', function(obj) {
-    return typeof obj == 'string'
-  })
-
-  // smart-ish replace filter, which works on strings and objects
-  eleventyConfig.addFilter('replace', function(input, string, replace) {
-    let stringified = typeof input === 'object' ? JSON.stringify(input) : input;
-    let output = JSON.parse(replaceAll(stringified, string, replace));
-    return output;
-  })
-
-  // Modified version, original @https://stevenwoodson.com/blog/a-step-by-step-guide-to-sorting-eleventy-global-data-files-by-date/
-  eleventyConfig.addFilter("sortDataByDate", (obj, dateField="date", order="asc") => {
-    const sorted = {};
-
-    Object.keys(obj)
-      .sort((a,b) => {
-        let sortOrder = order === 'desc' ? obj[a][dateField] > obj[b][dateField] : obj[b][dateField] > obj[a][dateField];
-        return sortOrder ? 1 : -1;
-      })
-      .forEach((name) => (sorted[name] = obj[name]));
-    return sorted;
-  });
-
-  eleventyConfig.addPlugin(pluginRev);
+  eleventyConfig.addDataExtension('yaml', (contents) => yaml.load(contents));
+  eleventyConfig.addDataExtension('yml', (contents) => yaml.load(contents));
 
   eleventyConfig.addPlugin(eleventySass, {
     compileOptions: {
-      permalink: function(contents, inputPath) {
-        return (data) => `${data.page.filePathStem.replace(/^\/scss\//, "/css/")}.css`;
-      }
+      permalink: function (contents, inputPath) {
+        return (data) =>
+          `${data.page.filePathStem.replace(/^\/scss\//, '/css/')}.css`;
+      },
     },
     sass: {
-      style: "compressed",
-      sourceMap: true
+      style: 'compressed',
+      sourceMap: true,
     },
-    rev: true
+    rev: true,
+  });
+
+  eleventyConfig.addCollection('catalog', (collection, arg) => {
+    return collection.getAll()[0].data.catalog;
   });
 
   eleventyConfig.setServerOptions({
@@ -148,23 +75,22 @@ module.exports = function(eleventyConfig) {
     },
 
     // Change the default file encoding for reading/serving files
-    encoding: "utf-8",
+    encoding: 'utf-8',
 
     // Show the dev server version number on the command line
     showVersion: true,
   });
 
-
   eleventyConfig.setQuietMode(true);
-  
+
   return {
-    htmlTemplateEngine: "njk",
+    htmlTemplateEngine: 'njk',
     passthroughFileCopy: true,
     dir: {
-      input: "src",
-      output: "dist",
-      includes: "_includes",  // default: "_includes"
-      data: "_data",          // default: "_data"
+      input: 'src',
+      output: 'dist',
+      includes: '_includes', // default: '_includes'
+      data: '_data', // default: '_data'
     },
   };
 };
