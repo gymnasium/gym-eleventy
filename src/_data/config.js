2e28923e09a1fd285e02962f4e55cdc33895e19f
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-01-03 16:48:58 -0500

    Try concatenated config for dynamic env variables
    YAML for static
    JS for dynamic

---
+++ src/_data/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root_url: process.env.ROOT_URL || 'https://gym.soy',
+  cms_url: process.env.CMS_URL || 'https://data.gym.soy',
+  lms_url: process.env.LMS_URL || 'https://learn.gym.soy',
+  mfe_url: process.env.MFE_URL || 'https://apps.learn.gym.soy',
+  lms_css_url: process.env.LMS_CSS_URL || 'https://gym.soy/css/theme.css',
+  mfe_css_url: process.env.MFE_CSS_URL || 'https://gym.soy/css/mfe.css',
+};
