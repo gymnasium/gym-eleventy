873e81b7e5a227532501b00b128f479e76d58b16
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-02-16 11:36:18 -0500

    update 11ty-images input path

--- 11ty.config/images.js
+++ 11ty.config/images.js
@@ -12,10 +12,13 @@ module.exports = eleventyConfig => {
   // Eleventy Image shortcode
   // https://www.11ty.dev/docs/plugins/image/
   eleventyConfig.addAsyncShortcode("image", async function imageShortcode(src, alt, widths, sizes) {
+    // 
+    const file = path.resolve(`${eleventyConfig.dir.input}/img/${src}`);
+
     // Full list of formats here: https://www.11ty.dev/docs/plugins/image/#output-formats
     // Warning: Avif can be resource-intensive so take care!
     let formats = ["avif", "webp", "auto"];
-    let file = relativeToInputPath(this.page.inputPath, src);
+
     let metadata = await eleventyImage(file, {
       widths: widths || ["auto"],
       formats,
