5dbe46ad90671b081b142b3e374563d6bcaa27a0
Roman Edirisinghe <rediris@users.noreply.github.com>
2024-04-25 13:55:44 -0400

    FOUT trick for MFE CSS, update env, update course_endpoint filter

--- 11ty.config/filters.js
+++ 11ty.config/filters.js
@@ -160,10 +160,10 @@ module.exports = eleventyConfig => {
         // console.log(process.env);
 
         if (dest_format === 'course_about') {
-          path = `${destUrl}course-v1:GYM+${idString}+0/about`;
+          path = `${destUrl}/courses/course-v1:GYM+${idString}+0/about`;
           
         } else if (dest_format === 'learning') {
-          path = `${destUrl}course-v1:GYM+${idString}+0/home`;
+          path = `${destUrl}/learning/course/course-v1:GYM+${idString}+0/home`;
         }
       } else {
         path = `/courses/course-v1:GYM+${idString}+0/`;
--- src/_data/config.js
+++ src/_data/config.js
@@ -1,15 +1,17 @@
 module.exports = {
   urls: {
     root_domain: process.env.ROOT_DOMAIN || 'gym.soy',
+    base_domain: process.env.BASE_DOMAIN || 'gym.soy',
     root: process.env.ROOT_URL || 'https://gym.soy',
+    base_url: process.env.BASE_URL || 'https://gym.soy',
     data: process.env.DATA_URL || 'https://data.gym.soy',
     cms: process.env.CMS_URL || 'https://studio.learn.gym.soy',
     lms: process.env.LMS_URL || 'https://learn.gym.soy',
     mfe: process.env.MFE_URL || 'https://apps.learn.gym.soy',
     production_domain: 'thegymnasium.com',
     production: 'https://thegymnasium.com',
-    mfe_course_about_base_url: process.env.MFE_COURSE_ABOUT_BASE_URL || 'https://learn.gym.soy/courses/',
-    mfe_learning_base_url: process.env.MFE_LEARNING_BASE_URL || 'https://apps.learn.gym.soy/learning/course/',
+    mfe_course_about_base_url: process.env.MFE_COURSE_ABOUT_BASE_URL || 'https://learn.gym.soy',
+    mfe_learning_base_url: process.env.MFE_LEARNING_BASE_URL || 'https://apps.learn.gym.soy',
   },
   ports: {
     account: process.env.MFE_PORT_ACCOUNT || 1997,
